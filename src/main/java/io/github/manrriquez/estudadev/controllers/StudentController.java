09833f3751613ff316c8c02fb9c95a66d2637311
Manrriquez <manrriquez.contato@gmail.com>
2023-01-17 19:55:14 -0300

    feat: implementando metodo listar todos estudantes

--- src/main/java/io/github/manrriquez/estudadev/controllers/StudentController.java
+++ src/main/java/io/github/manrriquez/estudadev/controllers/StudentController.java
@@ -1,10 +1,25 @@
 package io.github.manrriquez.estudadev.controllers;
 
 
+import io.github.manrriquez.estudadev.data.vo.StudentVO;
+import io.github.manrriquez.estudadev.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/student/v1")
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<StudentVO> findAll() {
+        return studentService.findAll();
+    }
 }
--- src/main/java/io/github/manrriquez/estudadev/services/StudentService.java
+++ src/main/java/io/github/manrriquez/estudadev/services/StudentService.java
@@ -1,6 +1,8 @@
 package io.github.manrriquez.estudadev.services;
 
 
+import io.github.manrriquez.estudadev.data.vo.StudentVO;
+import io.github.manrriquez.estudadev.mapper.ModelMapper;
 import io.github.manrriquez.estudadev.repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,4 +18,8 @@ public class StudentService {
     private StudentRepository studentRepository;
     private final Logger logger = Logger.getLogger(StudentService.class.getName());
 
+    public List<StudentVO> findAll() {
+        logger.info("Listando todos estudantes cadastrados..");
+        return ModelMapper.parseListObjects(studentRepository.findAll(), StudentVO.class);
+    }
 }
