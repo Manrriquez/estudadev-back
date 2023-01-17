0d73dbe9eb8e4e6a5fa60ca3b754f8b356d57f18
Manrriquez <manrriquez.contato@gmail.com>
2023-01-17 20:11:11 -0300

    feat: finalizando demais metodos update, delete, read de entidade estudante

--- src/main/java/io/github/manrriquez/estudadev/controllers/StudentController.java
+++ src/main/java/io/github/manrriquez/estudadev/controllers/StudentController.java
@@ -5,9 +5,8 @@ import io.github.manrriquez.estudadev.data.vo.StudentVO;
 import io.github.manrriquez.estudadev.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,4 +22,24 @@ public class StudentController {
         return studentService.findAll();
     }
 
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public StudentVO findById(@PathVariable(value = "id") Long id) throws Exception {
+        return studentService.findById(id);
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public StudentVO create(@RequestBody StudentVO person) {
+        return studentService.create(person);
+    }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public StudentVO update(@RequestBody StudentVO person) {
+        return studentService.update(person);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+        studentService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/io/github/manrriquez/estudadev/services/StudentService.java
+++ src/main/java/io/github/manrriquez/estudadev/services/StudentService.java
@@ -1,8 +1,10 @@
 package io.github.manrriquez.estudadev.services;
 
 
+import io.github.manrriquez.estudadev.Exceptions.ResourceNotFoundException;
 import io.github.manrriquez.estudadev.data.vo.StudentVO;
 import io.github.manrriquez.estudadev.mapper.ModelMapper;
+import io.github.manrriquez.estudadev.models.StudentModel;
 import io.github.manrriquez.estudadev.repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,4 +25,34 @@ public class StudentService {
         return ModelMapper.parseListObjects(studentRepository.findAll(), StudentVO.class);
     }
 
+    public StudentVO findById(Long id) {
+        logger.info("Buscando id estudante..");
+        var entity = studentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("ID estudante não encontrado, tente novamente."));
+        return ModelMapper.parseObject(entity, StudentVO.class);
+    }
+
+    public StudentVO create(StudentVO student) {
+        logger.info("Criando entidade estudante..");
+        var entity = ModelMapper.parseObject(student, StudentModel.class);
+        var vo = ModelMapper.parseObject(studentRepository.save(entity), StudentVO.class);
+        return vo;
+    }
+
+    public StudentVO update(StudentVO person) {
+
+        logger.info("Atualizando Entidade Pessoa..");
+        var entity = studentRepository.findById(person.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("ID estudante não encontrado, tente novamente."));
+        var vo = ModelMapper.parseObject(studentRepository.save(entity), StudentVO.class);
+        return vo;
+    }
+
+    public void delete(Long id) {
+
+        logger.info("Deletando Entidade Pessoa..");
+        var entity = studentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("ID estudante não encontrado, tente novamente."));
+        studentRepository.delete(entity);
+    }
 }
